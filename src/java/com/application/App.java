3eebb4e355f21aadfbee0dd0eac00841a09f06f4
itsrinuhere <itsrinuhere@icloud.com>
2022-05-21 14:17:04 +0530

    intila upload

---
+++ src/java/com/application/App.java
@@ -0,0 +1,8 @@
+package com.application;
+public class App{
+    public static void main(String[] args){
+        JFrame f=new JFrame("Hello World");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
